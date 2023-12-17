b2629f5bfb151702112b47e6a16f973a982806e3
Anasyasia20 <114215694+Anasyasia20@users.noreply.github.com>
2023-12-17 21:24:24 +0300

    Create Cat_Object.py
    Exercise-21.7.1

---
+++ Cat_Object.py
@@ -0,0 +1,13 @@
+from Cat_Class import Cat
+
+cat1 = Cat("Барон", "Мальчик", 2)
+
+print("Имя 1 кота:", cat1.getName())
+print("Пол 1 кота:", cat1.getGender())
+print("Возраст 1 кота:",  cat1.getAge())
+
+cat2 = Cat("Сэм", "Мальчик", 2)
+
+print("Имя 2 кота:", cat2.getName())
+print("Пол 2 кота:", cat2.getGender())
+print("Возраст 2 кота:", cat2.getAge())
